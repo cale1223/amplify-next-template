da62a5aa1961afd860a03535f31bffc68d104b52
cale1223 <cale.mercer@gmail.com>
2024-08-25 17:43:53 +0930

    added authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { BiTrash } from "react-icons/bi";
 
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
@@ -37,27 +40,32 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} className="flex justify-between px-5 items-center">
-            {todo.content}
-            <BiTrash className="w-6 h-auto hover:text-red-600 cursor-pointer" onClick={() => deleteTodo(todo.id)} />
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a
-          href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/"
-          className="text-3xl"
-        >
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id} className="flex justify-between px-5 items-center">
+                {todo.content}
+                <BiTrash className="w-6 h-auto hover:text-red-600 cursor-pointer" onClick={() => deleteTodo(todo.id)} />
+              </li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a
+              href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/"
+              className="text-3xl"
+            >
+              Review next steps of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
